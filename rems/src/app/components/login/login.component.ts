c07a7d1403ba80a3740512ee9ec5dd51fff821d7
karantechietact-2004 <karan.techietact@gmail.com>
2025-12-08 22:12:46 +0530

    dashboard count
    dashboard count

--- rems/src/app/components/login/login.component.ts
+++ rems/src/app/components/login/login.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Login } from '../../models/login';
+import { ToastrService } from 'ngx-toastr';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-login',
@@ -11,20 +13,36 @@ import { Login } from '../../models/login';
 })
 export class LoginComponent {
 
-  login:Login = new Login();
+  login: Login = new Login();
 
   private readonly validEmail = 'admin@gmail.com';
   private readonly validPassword = 'admin123';
 
-  constructor(private router: Router) {}
+  constructor(
+    private router: Router,
+    private toastr: ToastrService,
+    private spinner: NgxSpinnerService
+  ) {}
 
   onSubmit(form: NgForm) {
-    if (this.login.email === this.validEmail && this.login.password === this.validPassword) {
-      alert('Login Successfully!');
-      this.router.navigate(['/dashboard']); // redirect after success
-    } else {
-      alert(' Invalid Email or Password');
-    }
-  }
 
+    this.spinner.show(); // Show loading
+
+    setTimeout(() => { // simulate server delay
+
+      if (this.login.email === this.validEmail && this.login.password === this.validPassword) {
+        
+        this.toastr.success('Login Successful');
+        this.spinner.hide();
+
+        this.router.navigate(['/dashboard']);
+
+      } else {
+
+        this.toastr.error('Invalid Email or Password');
+        this.spinner.hide();
+      }
+
+    }, 1500); // 1.5 sec loading effect
+  }
 }
